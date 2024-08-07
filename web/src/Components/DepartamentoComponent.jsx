92f29b214617e7a175a2ddcf88920de0aba34aa8
Daniel Duarte <danielduartefilho.df@gmail.com>
2024-08-07 17:47:21 -0300

    removido itens que não serão utilizados

--- web/src/Components/DepartamentoComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function DepartmentoComponent() {
-  return <div>Conteúdo exclusivo para Departamento</div>;
-}
-
-export default DepartmentoComponent;
--- web/src/Components/UserComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function UserComponent() {
-  return <div>Conteúdo exclusivo para Usuários</div>;
-}
-
-export default UserComponent;
