b0503d041d65f93a35624591ed9aa2a8867167f0
Daniel Duarte <danielduartefilho.df@gmail.com>
2024-08-07 21:25:36 -0300

    adicionado função de consulta ao cep na service

--- web/src/Pages/Register/Register.jsx
+++ web/src/Pages/Register/Register.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styles from "./Register.module.css";
 import logo from "../../Assets/logo.png";
-import { cpf, cnpj } from 'cpf-cnpj-validator';
 import {
   TextField, Grid, Paper
 } from '@material-ui/core';
