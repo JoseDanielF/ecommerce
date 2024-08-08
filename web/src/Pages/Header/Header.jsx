c06ae228f644052d840cfbcec1633062d2420e72
Daniel Duarte <danielduartefilho.df@gmail.com>
2024-08-08 20:24:19 -0300

    atualizado header com carrinho de compras

--- web/src/Pages/Header/Header.jsx
+++ web/src/Pages/Header/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaUserCircle } from 'react-icons/fa';
+import { FaUserCircle, FaShoppingCart } from 'react-icons/fa';
 import { IoMdExit } from "react-icons/io";
 import Styles from '../Header/Header.module.css';
 import dadosUserLogadoService from '../../Services/DadosUserLogado/DadosUserLogado-service';
 
 const Header = () => {
+  const [cartItemCount, setCartItemCount] = useState(3);
   const navigate = useNavigate();
 
   const goToProfile = () => {
@@ -21,8 +22,18 @@ const Header = () => {
     <div className={Styles.headerContainer}>
       <h1 className={Styles.title}>Ecommerce</h1>
       <div className={Styles.iconContainer}>
-        <FaUserCircle className={Styles.profileIcon} onClick={goToProfile} />
-        <IoMdExit className={Styles.profileIcon} onClick={realizarLogout} />
+        <div className={Styles.iconBox} onClick={goToProfile}>
+          <FaUserCircle className={Styles.profileIcon} />
+        </div>
+        <div className={Styles.iconBox}>
+          <FaShoppingCart className={Styles.profileIcon} />
+          {cartItemCount > 0 && (
+            <div className={Styles.cartCount}>{cartItemCount}</div>
+          )}
+        </div>
+        <div className={Styles.iconBox} style={{marginRight: 15}} onClick={realizarLogout}>
+          <IoMdExit className={Styles.profileIcon} />
+        </div>
       </div>
     </div>
   );
