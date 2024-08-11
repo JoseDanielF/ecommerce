b6158c31f89d479ed10e0173c9859349be527219
Daniel Duarte <danielduartefilho.df@gmail.com>
2024-08-11 12:14:28 -0300

    atualizado header com a quantidade de itens no carrinho

--- web/src/Pages/Header/Header.jsx
+++ web/src/Pages/Header/Header.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaUserCircle, FaShoppingCart } from 'react-icons/fa';
 import { IoMdExit } from "react-icons/io";
 import Styles from '../Header/Header.module.css';
 import dadosUserLogadoService from '../../Services/DadosUserLogado/DadosUserLogado-service';
 
-const Header = () => {
-  const [cartItemCount, setCartItemCount] = useState(3);
+const Header = ({ cartItemCount }) => {
   const navigate = useNavigate();
 
   const goToProfile = () => {
